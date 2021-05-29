efc482c783da63af08242f3e2c85d51e5362f7a2
aguscorvo <aguscorvo@gmail.com>
2021-05-28 23:49:21 -0300

    Input container styles for mobile

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,22 @@
 import { getWeatherByLocation, deleteContent } from './weather.js';
 import { successCallback, errorCallback } from './geolocation.js';
+// import * as L from 'leaflet';
 export let locationInput = document.querySelector('.location');
 let searchBtn = document.querySelector('.search');
 let deleteBtn = document.querySelector('.delete');
 searchBtn.addEventListener('click', () => getWeatherByLocation(locationInput.value));
 deleteBtn.addEventListener('click', () => deleteContent());
 navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+//Mapa
+// let map = L.map('map', {
+//   minZoom: 3,
+//   maxZoom: 11,
+// }).setView([-34.89014879874051, -56.166746954884836], 3);
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//   attribution:
+//     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+// }).addTo(map);
+// let marker = L.marker([-34.9, -56])
+//   .addTo(map)
+//   .bindPopup('Estás acá')
+//   .openPopup();
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { getWeatherByLocation, deleteContent } from './weather.js';
 import { successCallback, errorCallback } from './geolocation.js';
+// import * as L from 'leaflet';
 
 export let locationInput: HTMLInputElement =
   document.querySelector('.location');
@@ -13,3 +14,20 @@ searchBtn.addEventListener(
 deleteBtn.addEventListener('click', (): void => deleteContent());
 
 navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+//Mapa
+
+// let map = L.map('map', {
+//   minZoom: 3,
+//   maxZoom: 11,
+// }).setView([-34.89014879874051, -56.166746954884836], 3);
+
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//   attribution:
+//     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+// }).addTo(map);
+
+// let marker = L.marker([-34.9, -56])
+//   .addTo(map)
+//   .bindPopup('Estás acá')
+//   .openPopup();
